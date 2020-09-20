4562b5e361c08cc2ef784173d99e1599b7524163
Emilio Bello <emiliobello@Emilios-MBP.zyxel.com>
2020-09-20 01:18:36 -0500

    function to display all the data and filtered. Need to polish the active filter display vs default display

--- static/js/app.js
+++ static/js/app.js
@@ -11,15 +11,15 @@ var form = d3.select('.form-group');
 var button = d3.select('#filter-btn');
 
 // creating event handlers
-button.on('click', runEnter);
+button.on('click', runEnter); // function inside that activates default display of input blank and if not the filter?
 form.on('click', runEnter); // is this affecting all the forms?
 
-// data displayed when no filters are active
+
 
 
 // data displayed when the filters are active
 function runEnter() {
-
+    
     // preventing the page from refreshing
     d3.event.preventDefault();
 
@@ -32,13 +32,27 @@ function runEnter() {
     // getting the filtered-by-date data from the input date
     var filteredDate = tableData.filter(date => date.datetime === DateValue);
 
-    // filteredDate.forEach((sighting) => {
-    //     var row = tbody.append('tr');
-    //     Object.defineProperties(sighting).forEach(([key, value]) => {
-    //         var cell = row.append('td');
-    //         cell.text(value);
-    //     });
-    // });
-    return console.log(filteredDate);
+    if (DateValue !== '') {
+        // populate the table with the filtered data
+        filteredDate.forEach((sighting) => {
+            var row = tbody.append('tr');
+            Object.entries(sighting).forEach(([key, value]) => {
+                var cell = row.append('td');
+                cell.text(value);
+            });
+        });
+    }
+    else {
+        // data displayed when no filters are active
+        tableData.forEach((sighting) => {
+            var row = tbody.append('tr');
+            Object.entries(sighting).forEach(([key, value]) => {
+                var cell = row.append('td');
+                cell.text(value);
+            });
+        });
+    }
+    
+    console.log(filteredDate);
 
 };
