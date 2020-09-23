6d406e6379064b4c94134b117ab440f1751081fd
Emilio Bello <emiliobello@Emilios-MBP.zyxel.com>
2020-09-23 10:01:36 -0500

    changes in the code. Multiple fields approach by building a dict of val and filtering.

--- static/js/app.js
+++ static/js/app.js
@@ -4,55 +4,74 @@ console.log(tableData)
 // getting the reference to the table body
 var tbody = d3.select('tbody')
 
-// selecting the form group
-var form = d3.select('.form-group');
+// // selecting the form group
+// var form = d3.select('#form');
 
-// selecting the button that filters the information
-var button = d3.select('#filter-btn');
+// // selecting the button that filters the information
+// var button = d3.select('#filter-btn');
 
-// creating event handlers
-button.on('click', runEnter); // function inside that activates default display of input blank and if not the filter?
-form.on('click', runEnter); // is this affecting all the forms?
+// // creating event handlers for the button
+// button.on('click', runEnter); 
+// form.on('change', runEnter); 
+
+// function to display the data table
+function displayTable(data) {    // added data within the function
+    
+    // clearing any existing data
+    tbody.html(""); // added
+
+    tableData.forEach((sighting) => {
+        var row = tbody.append('tr');
+        Object.entries(sighting).forEach((value) => {
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    })
+};
 
 
+create an empty dict of all filters
+var 
 
-
-// data displayed when the filters are active
+// function to display the table when the filters are active
 function runEnter() {
     
     // preventing the page from refreshing
     d3.event.preventDefault();
 
-    // selecting the inpput elemnt and getting the raw HTML note
-    var inputDate = d3.select('#datetime');
+    // // selecting the inpput elements and getting the raw HTML node
+    var inputData = d3.selectAll('input');
+    
+    // getting the value property of the input element and changing it to lower case (for fields other than date)
+    var inputValues = inputData.property('value').trim().toLowerCase();
+    console.log(inputValues);
+    
+    // getting the filtered-by-value data from the input entered
+    var filteredData = tableData.filter(data => data.datetime === inputValues ||
+                                                data.city === inputValues ||
+                                                data.state === inputValues ||
+                                                data.country === inputValues ||
+                                                data.shape === inputValues);
+    // dictionary of filters, a function that checks the input if it changed or not
+    // then if it changed, add it to the filtered values   
+    //console.log(filteredData);
 
-    // getting the value property of the input element
-    var DateValue = inputDate.property('value');
+    if (inputValues) {
+        // Clearing the loaded data to prevent appending
+        tbody.html("");
 
-    // getting the filtered-by-date data from the input date
-    var filteredDate = tableData.filter(date => date.datetime === DateValue);
-
-    if (DateValue !== '') {
         // populate the table with the filtered data
-        filteredDate.forEach((sighting) => {
+        filteredData.forEach((sighting) => {
             var row = tbody.append('tr');
             Object.entries(sighting).forEach(([key, value]) => {
                 var cell = row.append('td');
                 cell.text(value);
             });
-        });
+        })
     }
     else {
-        // data displayed when no filters are active
-        tableData.forEach((sighting) => {
-            var row = tbody.append('tr');
-            Object.entries(sighting).forEach(([key, value]) => {
-                var cell = row.append('td');
-                cell.text(value);
-            });
-        });
+        displayTable(data);
     }
-    
-    console.log(filteredDate);
-
 };
+
+displayTable(data);
